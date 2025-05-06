be2fb24ec8c44312abfcbecefe5ffc23c12cd101
isaiasrdgjr <isaias.rdg@gmail.com>
2025-05-06 18:54:58 -0300

    Alterar atividade1

--- aula_07/atividade1.py
+++ aula_07/atividade1.py
@@ -1,11 +1,14 @@
 def multiplos(x):
-    d = x * 2
-    t = x * 3
-    q = x ** 2
-    print(f'O dobro do número é: {d}')
-    print(f'O triplo do número é: {t}')
-    print(f'O quadrado do número é: {q}')
+    dobro = x * 2
+    triplo = x * 3
+    quadrado = x ** 2
+
+    return dobro, triplo, quadrado    
 
 
 num = int(input('Informe um número: '))
-multiplos_num = multiplos(num)
+d, t, q = multiplos(num)
+
+print(f'O dobro do número é: {d}')
+print(f'O triplo do número é: {t}')
+print(f'O quadrado do número é: {q}')
