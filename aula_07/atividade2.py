3e4b22c37c3039f8055845033bf0dda18b642f91
isaiasrdgjr <isaias.rdg@gmail.com>
2025-05-06 19:13:53 -0300

    Alterar variável da atividade2

--- aula_07/atividade2.py
+++ aula_07/atividade2.py
@@ -1,11 +1,12 @@
 pesopeixes = float(input('Informe o peso total de peixes pescados: '))
 
 PESO_LIMITE = 100.0
+MULTA_KG = 4.0
 
 if pesopeixes > PESO_LIMITE:
-    def multaexcesso(x):
-        e = x - PESO_LIMITE
-        m = e * 4.0
+    def multaexcesso(p):
+        e = p - PESO_LIMITE
+        m = e * MULTA_KG
         return m
     multa = multaexcesso(pesopeixes)
     print(f'O peso de peixes excedeu o limite, você recebeu uma multa de: R${multa:.2f}')
